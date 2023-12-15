6245bbbbef3ba220ce634fa7131ce0161a7ecfd8
MaTTalv001 <pomeranian1981@gmail.com>
2023-12-15 21:48:54 +0900

    関西弁モード搭載

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,14 +4,13 @@ class BoardsController < ApplicationController
   end
   
   def index
-    current_date = Date.today
-    formatted_date = current_date.strftime('%m月%d日')
-    begin
-
-      @chatgpt = ChatgptService.call("簡単に挨拶してください。その際、#{formatted_date}が誕生日の有名人を簡単に紹介してください。")
-    rescue Net::ReadTimeout
-      @chatgpt = "ようこそ！今日は#{formatted_date}です。"
-    end
+    # current_date = Date.today
+    # formatted_date = current_date.strftime('%m月%d日')
+    # begin
+      # @chatgpt = ChatgptService.call("簡単に挨拶してください。その際、#{formatted_date}が誕生日の有名人を簡単に紹介してください。")
+    # rescue Net::ReadTimeout
+      # @chatgpt = "ようこそ！今日は#{formatted_date}です。"
+    # end
     @boards = Board.all.order(created_at: :desc)
     @board = Board.new 
   end
@@ -22,6 +21,12 @@ class BoardsController < ApplicationController
 
   def create
     @board = Board.new(board_params)
+    if params[:board][:western_mode] == '1'
+      begin
+        @board.body = ChatgptService.call("次の入力文を関西弁にしてください 入力文：#{@board.body}")
+      rescue Net::ReadTimeout
+      end
+    end
     if @board.save
       redirect_to boards_path, success: I18n.t('defaults.message.created', item: Board.model_name.human)
     else
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,5 @@
 class Board < ApplicationRecord
   validates :title, length: { maximum: 255 }, presence: true
   validates :body, length: { maximum: 65_535 }, presence: true
+  attr_accessor :eastern_mode # 仮想属性としてeastern_modeを追加
 end
